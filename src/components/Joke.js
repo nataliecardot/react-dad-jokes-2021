27a0d08d73853f4ab645ea36e333095c5677a17e
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-21 16:46:29 -0700

    Fetch joke

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Joke() {
+  const [joke, setJoke] = useState('');
+
+  // useEffect: By using this Hook, you tell React that your component needs to do something after render. By default, useEffect runs after every render. Passing in an empty array [] of inputs tells React that your effect doesn’t depend on any values from the component, so that effect would run only on mount and clean up on unmount; it won’t run on updates
+  useEffect(() => {
+    axios
+      .get('https://icanhazdadjoke.com/', {
+        headers: { Accept: 'application/json' },
+      })
+      .then((res) => setJoke(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="card w-50 mx-auto mt-5">
       <h5 className="card-header">Dad Joke</h5>
       <div className="card-body">
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
+        <p className="card-text">{joke.joke}</p>
         <a href="#" className="btn btn-primary">
           Get Another Joke
         </a>
