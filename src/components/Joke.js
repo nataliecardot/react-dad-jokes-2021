e5116452c082a8c6b33620aa719f152af0b3688c
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-21 16:53:23 -0700

    Enable fetch new joke button

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -14,12 +14,21 @@ export default function Joke() {
       .catch((err) => console.log(err));
   }, []);
 
+  const fetchJoke = () => {
+    axios
+      .get('https://icanhazdadjoke.com/', {
+        headers: { Accept: 'application/json' },
+      })
+      .then((res) => setJoke(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="card w-50 mx-auto mt-5">
       <h5 className="card-header">Dad Joke</h5>
       <div className="card-body">
         <p className="card-text">{joke.joke}</p>
-        <a href="#" className="btn btn-primary">
+        <a className="btn btn-primary text-white" onClick={fetchJoke}>
           Get Another Joke
         </a>
       </div>
