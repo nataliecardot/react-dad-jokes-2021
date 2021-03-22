2aa4c24d4f85b27f76115cfccb076c95fc2438c7
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-21 17:22:21 -0700

    Add About component

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="text-center mt-5">
+      <h1>This is a React.js application.</h1>
+      <p className="mt-3 lead">It uses function components with React Hooks.</p>
+    </div>
+  );
+}
